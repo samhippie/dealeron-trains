0cca74b1ff30b38b3c5f84d993c8e02f77237577
Sam Higginbotham <samhggnbthm@gmail.com>
2020-07-31 10:43:30 -0500

    Adds min heap

---
+++ dealeron-trains.tests/MinHeapTests.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dealeron_trains.tests
+{
+    [TestFixture]
+    public class MinHeapTests
+    {
+        [Test]
+        public void MinHeap_OrdersNumbersShort()
+        {
+            MinHeap_OrdersNumbers(new List<int> { 6, 1, 9, 45 });
+        }
+
+        [Test]
+        public void MinHeap_OrdersNumbersMedium()
+        {
+            MinHeap_OrdersNumbers(new List<int> { 20, 4, 2, 4, -1, 1, 7, 2 });
+        }
+
+        [Test]
+        public void MinHeap_OrdersNumbersLong()
+        {
+            var numbers = new List<int> { 4, 2, 6, 21, 4, 1, 7, 9, 2, 45, 84 };
+            //add enough numbers to force a resize
+            for (var i = 0; i < 100; i++)
+            {
+                numbers.Add(i);
+            }
+            MinHeap_OrdersNumbers(numbers);
+        }
+
+        private void MinHeap_OrdersNumbers(List<int> numbers)
+        {
+            var heap = new MinHeap<int, string>();
+            var unique = 0; // the values have to all be unique
+            foreach (var number in numbers)
+            {
+                heap.Insert(number, $"number {number} ({unique++})");
+            }
+
+            var output = new List<string>();
+            while (heap.Length > 0)
+            {
+                output.Add(heap.Extract()?.value);
+            }
+
+            Assert.AreEqual(numbers.Count, output.Count);
+
+            numbers.Sort();
+
+            for (var i = 0; i< numbers.Count; i++)
+            {
+                Assert.IsTrue(output[i].StartsWith($"number {numbers[i]}"));
+            }
+        }
+
+        [Test]
+        public void MinHeap_DecreaseKeys1()
+        {
+            var heap = new MinHeap<int, string>();
+            heap.Insert(2, "a");
+            heap.Insert(3, "b");
+            heap.Insert(4, "c");
+            heap.Insert(1, "d");
+
+            heap.DecreaseKey("c", 0);
+
+            var min = heap.Extract();
+            Assert.AreEqual(0, min.Value.key);
+            Assert.AreEqual("c", min.Value.value);
+        }
+
+        [Test]
+        public void MinHeap_DecreaseKeys2()
+        {
+            var heap = new MinHeap<int, string>();
+            heap.Insert(2, "a");
+            heap.Insert(4, "b");
+            heap.Insert(5, "c");
+            heap.Insert(10, "d");
+            heap.Insert(8, "e");
+            heap.Insert(2, "f");
+
+            heap.DecreaseKey("b", 3);
+
+            var a = heap.Extract();
+            var f = heap.Extract();
+            //a and f might be swapped because they have the same key
+            if (a.Value.value == "f")
+            {
+                (a, f) = (f, a);
+            }
+            var b = heap.Extract();
+
+            Assert.AreEqual(2, a.Value.key);
+            Assert.AreEqual("a", a.Value.value);
+            Assert.AreEqual(2, f.Value.key);
+            Assert.AreEqual("f", f.Value.value);
+            Assert.AreEqual(3, b.Value.key);
+            Assert.AreEqual("b", b.Value.value);
+        }
+    }
+}
---
+++ dealeron-trains/MinHeap.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dealeron_trains
+{
+    //Using a library would make more sense for real code, but I like to limit the dependencies for take-home code tests like this
+    public class MinHeap<TKey, TValue> where TKey: IComparable
+    {
+        private (TKey key, TValue value)[] data = new (TKey, TValue)[33];
+        private int free = 1;
+        //keeps track of where a given value is
+        //this way we can easily decrease the key of a given value rather than needing to search the array
+        private readonly Dictionary<TValue, int> reverseIndex = new Dictionary<TValue, int>();
+
+        public int Length => free - 1;
+
+        public void Insert(TKey key, TValue value)
+        {
+            if (reverseIndex.ContainsKey(value))
+                throw new ArgumentException("Value has already been inserted");
+            data[free] = (key, value);
+            reverseIndex[value] = free;
+            free++;
+            if (free >= data.Length)
+            {
+                var newData = new (TKey, TValue)[data.Length * 2 - 1];
+                data.CopyTo(newData, 0);
+                data = newData;
+            }
+            DecreaseKeyByIndex(free - 1, key);
+        }
+
+        public void DecreaseKey(TValue value, TKey key)
+        {
+            DecreaseKeyByIndex(reverseIndex[value], key);
+        }
+
+        private void DecreaseKeyByIndex(int index, TKey key)
+        {
+            data[index].key = key;
+            var parent = Parent(index);
+            while (index > 1 && parent.HasValue && data[parent.Value].key.CompareTo(data[index].key) > 0)
+            {
+                SwapIndices(index, parent.Value);
+                index = parent.Value;
+                parent = Parent(index);
+            }
+        }
+
+        public (TKey key, TValue value)? Extract()
+        {
+            if (free <= 1)
+                return null;
+
+            var top = data[1];
+            data[1] = data[free-1];
+            free--;
+            reverseIndex[data[1].value] = 1;
+            Heapify(1);
+
+            return top;
+        }
+
+        private void Heapify(int index)
+        {
+            if (index >= free)
+                return;
+            var left = Left(index);
+            var right = Right(index);
+            //pick min between left and right
+            //have to be a little verbose because TKey might not be nullable
+            if (left.HasValue && data[left.Value].key.CompareTo(data[index].key) < 0 && 
+                (!right.HasValue || data[left.Value].key.CompareTo(data[right.Value].key) <= 0))
+            {
+                SwapIndices(index, left.Value);
+                Heapify(left.Value);
+            }
+            else if (right.HasValue && data[right.Value].key.CompareTo(data[index].key) < 0 &&
+                (!left.HasValue || data[right.Value].key.CompareTo(data[left.Value].key) <= 0))
+            {
+                SwapIndices(index, right.Value);
+                Heapify(right.Value);
+            }
+        }
+
+        private void SwapIndices(int i, int j)
+        {
+            (data[i], data[j]) = (data[j], data[i]);
+            reverseIndex[data[i].value] = i;
+            reverseIndex[data[j].value] = j;
+        }
+
+        private int? Left(int index)
+        {
+            var left = index * 2;
+            if (left < free)
+                return left;
+            else
+                return null;
+        }
+
+        private int? Right(int index)
+        {
+            var right = index * 2 + 1;
+            if (right < free)
+                return right;
+            else
+                return null;
+        }
+
+        private int? Parent(int index)
+        {
+            var parent = index / 2;
+            if (parent < free && parent >= 1)
+                return parent;
+            else
+                return null;
+        }
+    }
+}
